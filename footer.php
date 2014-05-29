74fa8136d2d4836609f189f6c917185dc9eb3ba2
antirez <antirez@gmail.com>
2014-05-29 12:07:40 +0200

    Fix Redis link into footer.

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div id="footer">Redis is a very simple Twitter clone written in PHP as example application of the <a href="http://code.google.com/p/redis/">Redis key-value database</a></div>
+<div id="footer">Redis is a very simple Twitter clone written in PHP as example application of the <a href="http://redis.io">Redis database</a></div>
 </div>
 </body>
 </html>
